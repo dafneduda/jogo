717cb7d90caa3c8562ec7f149fe6fb0a51ad4fda
Dafne Duda <dafne.leslie@gmail.com>
2025-06-16 23:06:02 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
