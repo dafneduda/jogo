7ff46581589e069b00b4adb44c61d1c5a4c1841b
Dafne Duda <dafne.leslie@gmail.com>
2025-07-05 21:33:32 -0300

    criação menu

--- code/game.py
+++ code/game.py
@@ -1,18 +1,15 @@
 import pygame
+
+from code.const import WIN_WIDTH, WIN_HEIGHT
 from code.menu import Menu
+
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode((800, 600))
+        self.window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
         while True:
             menu = Menu(self.window)
             menu.run()
-
-            # Checando eventos
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         pygame.quit()
-            #         exit()
